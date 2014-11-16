caf61ee2f0bc5ee1be8d765b21e1e7a92e432919
Sam Crang <sam.crang@gmail.com>
2014-11-16 21:40:30 +0000

    Add tests for remove action

--- spec/unit/provider_spec.rb
+++ spec/unit/provider_spec.rb
@@ -1,4 +1,6 @@
 require 'spec_helper'
+require '../libraries/chocolatey_helpers.rb'
+
 
 describe 'chocolatey_test::install_package' do
   context 'on a supported OS' do
@@ -16,6 +18,26 @@ describe 'chocolatey_test::install_package' do
 
     it 'installs package at a specific version' do
       expect(chef_run).to run_execute('install package test-package-version version 0.0.1').with(command: /[^\"].+\/bin\/choco\" install test-package-version -version 0.0.1/)
+    end
+  end
+end
+
+describe 'chocolatey_test::remove_package' do
+  context 'on a supported OS' do
+    let(:chef_run) {
+      ChefSpec::SoloRunner.new(
+        platform: 'windows',
+        version: '2012',
+        step_into: 'chocolatey'
+      )
+    }
+
+    it 'removes package' do
+      chef_run.converge(described_recipe) do
+        ChocolateyHelpers.stub(:package_installed?).and_return(true)
+        ChocolateyHelpers.stub(:package_exists?).and_return(true)
+      end
+      expect(chef_run).to run_execute('uninstall package test-package').with(command: /[^\"].+\/bin\/choco\" uninstall test-package/)
     end
   end
 end
